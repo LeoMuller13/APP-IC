68af7329c329f8a3b229013d06de9b82ecccbada
Leonardo Müller <95294915+LeoMuller13@users.noreply.github.com>
2022-09-18 22:43:41 -0300

    Add files via upload

---
+++ App.js
@@ -0,0 +1,11 @@
+import { StatusBar } from 'expo-status-bar';
+import Routes from './src/routes';
+import {NavigationContainer} from '@react-navigation/native'
+
+export default function App() {
+  return (
+    <NavigationContainer>
+      <Routes></Routes>
+    </NavigationContainer>
+  );
+}
